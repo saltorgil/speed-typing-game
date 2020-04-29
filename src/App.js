765b0fbdf1b91364956505313ebac5afa6a4a7a0
Salva Torru <salva339@gmail.com>
2020-04-29 15:26:53 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./app.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return(
+    <>
+      <div className='main-container'>
+        <h1>How fast do you type?</h1>
+        <textarea value=""/>
+        <h4>Time remaining:</h4>
+        <h1>Word Count</h1>
+      </div>
+    </>
+  )
+};
 
-export default App;
+export default App
