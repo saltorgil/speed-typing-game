2e42be585470326699ac96bca655965a07f5c93c
Salva Torru <salva339@gmail.com>
2020-04-29 15:24:30 +0100

    Updates

---
+++ precache-manifest.ca3e4683bd4e100ad212d98686657801.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7a19e733bb7c6d7d53fdd0e63db07b",
+    "url": "/speed-typing-game/index.html"
+  },
+  {
+    "revision": "e40abdb644fa15f9058d",
+    "url": "/speed-typing-game/static/css/main.194fc59a.chunk.css"
+  },
+  {
+    "revision": "90e71f4ba991f24ca80e",
+    "url": "/speed-typing-game/static/js/2.fe0e4576.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speed-typing-game/static/js/2.fe0e4576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e40abdb644fa15f9058d",
+    "url": "/speed-typing-game/static/js/main.9464f96c.chunk.js"
+  },
+  {
+    "revision": "8ef856c8f50bc7c09199",
+    "url": "/speed-typing-game/static/js/runtime-main.92673d00.js"
+  },
+  {
+    "revision": "419771618e9d300b04a63933612899b4",
+    "url": "/speed-typing-game/static/media/astron_boy-italic.41977161.ttf"
+  },
+  {
+    "revision": "f4d3a83e95c61129872d87993b9ace01",
+    "url": "/speed-typing-game/static/media/astron_boy.f4d3a83e.ttf"
+  },
+  {
+    "revision": "2fdb94fcea2f636bcace16ed34edea26",
+    "url": "/speed-typing-game/static/media/astron_boy_video.2fdb94fc.ttf"
+  },
+  {
+    "revision": "ae564d583629398ac20cd04aedf310b9",
+    "url": "/speed-typing-game/static/media/astron_boy_wonder.ae564d58.ttf"
+  }
+]);
