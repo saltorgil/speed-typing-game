898a1f439dadf2bf5b35f063d69b531d50c6f3e6
Salva Torru <salva339@gmail.com>
2020-04-29 17:51:13 +0100

    new styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "./app.css"
 
 function App() {
+
+  const [text, setText] = useState('')
+
+  const handleTextarea = (event) =>{
+    setText(event.target.value)
+  }
+
+
   return(
     <>
       <div className='main-container'>
         <h1>How fast do you type?</h1>
-        <textarea value=""/>
-        <h4>Time remaining:</h4>
-        <h1>Word Count</h1>
+        <textarea 
+          onChange={handleTextarea} 
+          value={text}
+        />
+        <h3>Time remaining:</h3>
+        <button>Start</button>
+        <h1>Word Count: ?</h1>
       </div>
     </>
   )
