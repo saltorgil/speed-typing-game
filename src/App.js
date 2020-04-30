ea9ab81c35454b7a9492378b5802cafac296b42c
Salva Torru <salva339@gmail.com>
2020-04-30 13:11:01 +0100

    words counter added

--- src/App.js
+++ src/App.js
@@ -4,29 +4,41 @@ import "./app.css"
 function App() {
 
   const [text, setText] = useState('')
-  const [timeRemaining, SetTimeRemaining] = useState(5)
+  const [timeRemaining, setTimeRemaining] = useState(3)
   const [isRunning, setIsRunning] = useState(false)
+  const [numWords, setNumWords] = useState(0)
 
   const handleTextarea = (event) =>{
     setText(event.target.value)
   }
 
+  const countTextWords = (text) => 
+    text
+      .trim()
+      .split(' ')
+      .filter(word=>word!=='')
+      .length
+
   const startGame = () => {
     setIsRunning(true)
+    setTimeRemaining(3)
+    setText('')
   }
 
   useEffect(()=>{
-      if (isRunning && timeRemaining > 0 ){
-        setTimeout(()=>{
-          SetTimeRemaining(prevTime => prevTime -1) 
-        },1000)
-      }
-      else if (timeRemaining === 0) {
-        setIsRunning(false);
-        console.log("juego terminado")
-      }
-        
-    },[timeRemaining, isRunning])
+
+    if (isRunning && timeRemaining > 0 ){
+      setTimeout(()=>{
+        setTimeRemaining(prevTime => prevTime -1) 
+      },1000)
+    }
+    else if (timeRemaining === 0) {
+      setIsRunning(false);
+      setNumWords(countTextWords(text));
+      console.log("juego terminado")
+    }
+
+  },[timeRemaining, isRunning])
 
 
   return(
@@ -39,7 +51,7 @@ function App() {
         />
         <h3>Time remaining: {timeRemaining}</h3>
         <button onClick={startGame}>Start</button>
-        <h1>Word Count:</h1>
+        <h1 onClick={countTextWords}>Word Count: {numWords}</h1>
       </div>
     </>
   )
