f03b447ccc43504c2b55fda884a2898036e21004
Salva Torru <salva339@gmail.com>
2020-04-30 12:05:22 +0100

    set time counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./app.css"
 
 function App() {
 
   const [text, setText] = useState('')
+  const [timeRemaining, SetTimeRemaining] = useState(5)
+  const [isRunning, setIsRunning] = useState(false)
 
   const handleTextarea = (event) =>{
     setText(event.target.value)
   }
+
+  const startGame = () => {
+    setIsRunning(true)
+  }
+
+  useEffect(()=>{
+      if (isRunning && timeRemaining > 0 ){
+        setTimeout(()=>{
+          SetTimeRemaining(prevTime => prevTime -1) 
+        },1000)
+      }
+      else if (timeRemaining === 0) {
+        setIsRunning(false);
+        console.log("juego terminado")
+      }
+        
+    },[timeRemaining, isRunning])
 
 
   return(
@@ -18,9 +37,9 @@ function App() {
           onChange={handleTextarea} 
           value={text}
         />
-        <h3>Time remaining:</h3>
-        <button>Start</button>
-        <h1>Word Count: ?</h1>
+        <h3>Time remaining: {timeRemaining}</h3>
+        <button onClick={startGame}>Start</button>
+        <h1>Word Count:</h1>
       </div>
     </>
   )
