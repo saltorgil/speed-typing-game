cab53d6e84545d382a2d5cffc0b3a3aad538d090
Salva Torru <salva339@gmail.com>
2020-05-05 17:20:17 +0100

    default sound to mute cause android browser issues

--- src/App.js
+++ src/App.js
@@ -1,54 +1,64 @@
-import React from "react";
-import useTypingGame from "./hooks/useTypingGame"
-import "./css/app.css" 
-import logo from "./images/logox2.png"
+import React from 'react';
+import useTypingGame from './hooks/useTypingGame';
+import './css/app.css';
+import logo from './images/logox2.png';
 
-import audio from "./intergalactic.ogg"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faVolumeUp, faVolumeMute } from '@fortawesome/free-solid-svg-icons'
-
+import audio from './intergalactic.ogg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVolumeUp, faVolumeMute } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const {
+    text,
+    timeRemaining,
+    isRunning,
+    randomQuote,
+    numWords,
+    refTextarea,
+    refAudio,
+    handleTextarea,
+    countTextWords,
+    startGame,
+    volumeOn,
+    handleVolumeOn,
+  } = useTypingGame(5);
 
-  const {text, timeRemaining, isRunning, randomQuote, numWords, refTextarea, refAudio,
-    handleTextarea, countTextWords, startGame, volumeOn, handleVolumeOn} = useTypingGame(5)
-
-
-  return(
-
-    <div className="main">
-      <audio ref={refAudio} autoPlay loop hidden>
-        <source src={audio} type="audio/ogg"/>
+  return (
+    <div className='main'>
+      <audio ref={refAudio} loop autoPlay hidden>
+        <source src={audio} type='audio/ogg' />
       </audio>
       <div className='main-container'>
-        <div className="main-container-logo"><img src={logo} alt="logo"></img></div>    
-        <hr/>
+        <div className='main-container-logo'>
+          <img src={logo} alt='logo'></img>
+        </div>
+        <hr />
 
         <h2>How fast do you type in only 5 seconds?</h2>
-        <h4>TRY THIS 	<span>&#9654; </span>{randomQuote}</h4>
-        <textarea 
-          onChange={handleTextarea} 
+        <h4>
+          TRY THIS <span>&#9654; </span>
+          {randomQuote}
+        </h4>
+        <textarea
+          onChange={handleTextarea}
           value={text}
-          disabled = {!isRunning}
-          ref = {refTextarea}
+          disabled={!isRunning}
+          ref={refTextarea}
         />
         <h3>Time remaining: {timeRemaining}</h3>
-        <button className="sheen"
-          onClick={startGame}
-          disabled = {isRunning}
-        >
-            Start
+        <button className='sheen' onClick={startGame} disabled={isRunning}>
+          Start
         </button>
         <h1 onClick={countTextWords}>Word Count: {numWords}</h1>
-        <FontAwesomeIcon 
+        <FontAwesomeIcon
           onClick={handleVolumeOn}
-          className="volume-icon" 
-          icon={volumeOn ? faVolumeUp: faVolumeMute} 
-          size= 'lg'
+          className='volume-icon'
+          icon={volumeOn ? faVolumeUp : faVolumeMute}
+          size='lg'
         />
       </div>
     </div>
-  )
-};
+  );
+}
 
-export default App
+export default App;
--- src/hooks/useTypingGame.js
+++ src/hooks/useTypingGame.js
@@ -1,103 +1,109 @@
-import  {useState, useEffect, useRef} from "react";
+import { useState, useEffect, useRef } from 'react';
 
-function useTypingGame(start_game_time){
+function useTypingGame(start_game_time) {
+  const [text, setText] = useState('');
+  const [timeRemaining, setTimeRemaining] = useState(start_game_time);
+  const [isRunning, setIsRunning] = useState(false);
+  const [numWords, setNumWords] = useState(0);
 
-    const [text, setText] = useState('')
-    const [timeRemaining, setTimeRemaining] = useState(start_game_time)
-    const [isRunning, setIsRunning] = useState(false)
-    const [numWords, setNumWords] = useState(0)
+  const [allQuotes, setAllQuotes] = useState([]);
+  const [randomQuote, setRandomQuote] = useState('');
 
-    const [allQuotes, setAllQuotes] = useState([])
-    const [randomQuote, setRandomQuote] = useState('')
+  const [volumeOn, setVolumeOn] = useState(false);
 
-    const [volumeOn, setVolumeOn] = useState(true)
+  const refTextarea = useRef(null);
+  const refAudio = useRef(null);
 
+  const handleVolumeOn = () => {
+    setVolumeOn((prevVolumeOn) => !prevVolumeOn);
+  };
 
-    const refTextarea = useRef(null);
-    const refAudio = useRef(null)
+  const handleTextarea = (event) => {
+    setText(event.target.value);
+  };
 
-    const handleVolumeOn = () => {
-      setVolumeOn(prevVolumeOn => !prevVolumeOn)
+  const countTextWords = (text) =>
+    text
+      .trim()
+      .split(' ')
+      .filter((word) => word !== '').length;
+
+  const startGame = () => {
+    setIsRunning(true);
+    setTimeRemaining(start_game_time);
+    setText('');
+    refTextarea.current.disabled = false;
+    refTextarea.current.focus();
+  };
+
+  const endGame = () => {
+    setIsRunning(false);
+    setNumWords(countTextWords(text));
+    getRandomQuote();
+  };
+
+  const getRandomQuote = () => {
+    const randNum = Math.floor(Math.random() * allQuotes.length);
+    setRandomQuote(allQuotes[randNum]);
+  };
+
+  useEffect(() => {
+    document.title = 'Speed Typing Game';
+
+    if (isRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      endGame();
+      console.log('juego terminado');
     }
-
-    const handleTextarea = (event) =>{
-      setText(event.target.value)
-    }
-  
-    const countTextWords = (text) => 
-      text
-        .trim()
-        .split(' ')
-        .filter(word=>word!=='')
-        .length
-  
-    const startGame = () => {
-      setIsRunning(true)
-      setTimeRemaining(start_game_time)
-      setText('')
-      refTextarea.current.disabled = false
-      refTextarea.current.focus()
-    }
-  
-    const endGame = () =>{
-      setIsRunning(false);
-      setNumWords(countTextWords(text));
-      getRandomQuote();
-    }
-
-    const getRandomQuote = () => {
-      const randNum = Math.floor(Math.random() * allQuotes.length)
-      setRandomQuote(allQuotes[randNum])
-    }
-  
-    useEffect(()=>{
-        document.title = "Speed Typing Game";
-  
-      if (isRunning && timeRemaining > 0 ){
-        setTimeout(()=>{
-          setTimeRemaining(prevTime => prevTime -1) 
-        },1000)
-      }
-      else if (timeRemaining === 0) {
-        endGame();
-        console.log("juego terminado")
-      }
     // eslint-disable-next-line
-    },[timeRemaining, isRunning])
+  }, [timeRemaining, isRunning]);
 
-
-    useEffect(()=>{
-
-      async function randomQuote() {
-        const response = await fetch('https://api.quotable.io/quotes?' + new URLSearchParams({
+  useEffect(() => {
+    async function randomQuote() {
+      const response = await fetch(
+        'https://api.quotable.io/quotes?' +
+          new URLSearchParams({
             maxLength: 55,
-            skip: Math.floor(Math.random() * 50)
-        }))
+            skip: Math.floor(Math.random() * 50),
+          })
+      );
 
-        const data = await response.json()
+      const data = await response.json();
 
-        const {results} = data
-        const arrayQuotes = results.map(result => result.content + " - " + result.author)
-        setAllQuotes(arrayQuotes);
-      }
-      
-      randomQuote()
+      const { results } = data;
+      const arrayQuotes = results.map(
+        (result) => result.content + ' - ' + result.author
+      );
+      setAllQuotes(arrayQuotes);
+    }
 
-    },[])
+    randomQuote();
+  }, []);
 
-    // eslint-disable-next-line
-    useEffect(()=>getRandomQuote(), [allQuotes])
+  // eslint-disable-next-line
+  useEffect(() => getRandomQuote(), [allQuotes]);
 
-    useEffect(()=>{
-      volumeOn ? refAudio.current.play() : refAudio.current.pause();
-    }, [volumeOn])
+  useEffect(() => {
+    volumeOn ? refAudio.current.play() : refAudio.current.pause();
+  }, [volumeOn]);
 
-
-
-
-    return {text, timeRemaining, isRunning, randomQuote, numWords, 
-      refTextarea, refAudio, handleTextarea, countTextWords, startGame,
-      volumeOn, handleVolumeOn}
+  return {
+    text,
+    timeRemaining,
+    isRunning,
+    randomQuote,
+    numWords,
+    refTextarea,
+    refAudio,
+    handleTextarea,
+    countTextWords,
+    startGame,
+    volumeOn,
+    handleVolumeOn,
+  };
 }
 
-export default useTypingGame
+export default useTypingGame;
